7f0ef0dcaa755a6fa5471ccdeedfc3034e049113
Aman <devproddeploy@gmail.com>
2020-09-20 11:09:54 +0530

    allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,13 @@
 function allLongestStrings(inputArray: string[]): string[] {
-
+    let maxLength = Number.MIN_SAFE_INTEGER;
+    inputArray.forEach(el => {
+        const elLength = el.length;
+        if (elLength > maxLength) {
+            maxLength = elLength;
+        }
+    })
+    const result = inputArray.filter(el => el.length === maxLength);
+    return result;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
