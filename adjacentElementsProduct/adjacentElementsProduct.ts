08081890478104724906b42b2fc22fb6251ee138
Aman <devproddeploy@gmail.com>
2020-09-20 10:58:29 +0530

    adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,14 @@
 function adjacentElementsProduct(inputArray: number[]): number {
-
+    let max: number = Number.MIN_SAFE_INTEGER;
+    let result: number = 0;
+    inputArray.forEach((num, i) => {
+        const product = num * inputArray[i + 1];
+        if (product > max) {
+            max = product;
+        }
+        result = max;
+    })
+    return result;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
