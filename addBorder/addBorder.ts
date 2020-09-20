8e7c98a5343a24f02af4996b1d5b76e4cb743517
Aman <devproddeploy@gmail.com>
2020-09-20 10:20:51 +0530

    add border

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,16 @@
 function addBorder(picture: string[]): string[] {
+    const hack = '*****';
+    const newPicture = picture.map(pic => {
+        const newPic = pic.split('');
+        newPic.push('*');
+        newPic.unshift('*');
+        return newPic.join('');
+    })
 
+    newPicture.push(hack);
+    newPicture.unshift(hack);
+
+    return newPicture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded", 'efg']));
