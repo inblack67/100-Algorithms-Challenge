850fa57281bc12978c7fb8295d8853d814786e61
Aman <devproddeploy@gmail.com>
2020-09-20 10:42:43 +0530

    add digits of a number

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -2,4 +2,4 @@ function addTwoDigits(n: number): number {
     return n.toString().split('').reduce((prev, curr) => +prev + +curr, 0);
 }
 
-console.log(addTwoDigits(29023));
+console.log(addTwoDigits(29));
