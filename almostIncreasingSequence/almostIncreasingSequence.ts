3a1d2f080cb6d8c894566ed7d27a241ae4086539
Aman <devproddeploy@gmail.com>
2020-09-24 09:37:42 +0530

    alphabeticShift

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,27 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+    let res = false;
+    let chances = 0;
+    sequence.forEach((el, i) => {
+        const next = sequence[i + 1];
+        const curr = el;
+        console.log(next)
+        console.log(curr)
+        if (next < curr) {
+            res = true;
+        }
+        else {
+            if (chances < 1) {
+                res = true;
+                chances += 1;
+            }
+            else {
+                res = false;
+            }
+        }
+    })
 
+    return res;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+// console.log(almostIncreasingSequence([1, 3, 2, 0]))
+console.log(almostIncreasingSequence([1, 3, 2]))
--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,11 @@
 function alphabeticShift(inputString: string): string {
-
+    const strArr: string[] = inputString.split('');
+    const res: string = strArr.map((char, i) => {
+        const w = inputString.charCodeAt(i) === 122 ? 97 : inputString.charCodeAt(i) + 1;
+        const newW = String.fromCharCode(w);
+        return newW;
+    }).join('');
+    return res;
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift('abcd'));
