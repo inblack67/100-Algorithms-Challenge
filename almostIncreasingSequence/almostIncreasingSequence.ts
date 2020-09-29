736f80b4d471187ef38ae667e95bf8783a140230
Aman <devproddeploy@gmail.com>
2020-09-29 21:41:11 +0530

    almost increasing

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,27 +1,15 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
-    let res = false;
-    let chances = 0;
-    sequence.forEach((el, i) => {
-        const next = sequence[i + 1];
-        const curr = el;
-        console.log(next)
-        console.log(curr)
-        if (next < curr) {
-            res = true;
-        }
-        else {
-            if (chances < 1) {
-                res = true;
-                chances += 1;
-            }
-            else {
-                res = false;
+    let count = 0;
+    sequence.forEach((num, i) => {
+        if (num <= sequence[i - 1]) {
+            count++;
+            if (sequence[i - 1] >= sequence[i + 1] && num <= sequence[i - 2]) {
+                count++;
             }
         }
     })
-
-    return res;
+    return count <= 1;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 0]))
+console.log(almostIncreasingSequence([1, 3, 2, 0]))
 console.log(almostIncreasingSequence([1, 3, 2]))
