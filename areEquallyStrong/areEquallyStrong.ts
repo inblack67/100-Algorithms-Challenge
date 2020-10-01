4d9bcddd92d196e8dbc97c5ebc62fbd091a358c1
Aman <devproddeploy@gmail.com>
2020-10-01 18:35:58 +0530

    areEquallyStrong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,11 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-
+    const yourStrongestArm = Math.max(yourLeft, yourRight);
+    const yourWeakestArm = Math.min(yourLeft, yourRight);
+    const friendsStrongestArm = Math.max(friendsLeft, friendsRight);
+    const friendsWeakestArm = Math.min(friendsLeft, friendsRight);
+    return (yourStrongestArm === friendsStrongestArm && yourWeakestArm === friendsWeakestArm);
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
