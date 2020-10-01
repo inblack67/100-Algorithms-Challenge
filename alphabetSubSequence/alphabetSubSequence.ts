1f1ab1959934f24e7338dbb1d443827fbd7d06bc
Aman <devproddeploy@gmail.com>
2020-10-01 11:22:26 +0530

    alphabetSubSequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,13 +1,28 @@
 function alphabetSubsequence(s: string): boolean {
     const strArray = s.split('');
     let chances = 0;
+    const codes = [];
+
+    // no duplicates
     strArray.forEach((char, i, arr) => {
+        const currentCode = s.charCodeAt(i);
+        codes.push(currentCode);
+    })
+
+    const codeSet = new Set(codes);
+
+    if (codeSet.size !== strArray.length) {
+        chances++;
+    }
+
+    // general check
+    for (let i = 0; i < strArray.length - 1; i++) {
         const currentCode = s.charCodeAt(i);
         const nextCode = s.charCodeAt(i + 1);
         if (currentCode >= nextCode) {
             chances++;
         }
-    })
+    }
     return chances === 0;
 }
 
