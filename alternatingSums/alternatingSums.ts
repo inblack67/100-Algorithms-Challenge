773040605a9b7ddf52f5239ce1831aef0425d910
Aman <devproddeploy@gmail.com>
2020-10-01 18:20:52 +0530

    alternatingSums2

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -2,6 +2,22 @@ function alternatingSums(a: number[]): number[] {
     const result = [];
     let w1 = 0, w2 = 0;
     a.forEach((num, i, arr) => {
+        if (i % 2 === 0) {
+            w1 += num;
+        }
+        else {
+            w2 += num;
+        }
+    })
+    result.push(w1);
+    result.push(w2);
+    return result;
+}
+
+function alternatingSums2(a: number[]): number[] {
+    const result = [];
+    let w1 = 0, w2 = 0;
+    a.forEach((num, i, arr) => {
         const team1 = arr[2 * i];
         const team2 = arr[2 * i + 1];
         if (team1 !== undefined) {
@@ -17,3 +33,4 @@ function alternatingSums(a: number[]): number[] {
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums2([50, 60, 60, 45, 70]))
