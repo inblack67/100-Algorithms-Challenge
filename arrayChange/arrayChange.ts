e28c188a46f0385d31ea6032adb9d348231794bc
Aman Bhardwaj <inblack>
2020-11-21 19:27:05 +0530

    array change

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,19 @@
-function arrayChange(inputArray: number[]): number {
- 
+function arrayChange ( inputArray: number[] ): number
+{
+    let count = 0;
+
+    inputArray.forEach( ( el, i, arr ) =>
+    {
+        if ( el >= arr[ i + 1 ] )
+        {
+            const diff = el - arr[ i + 1 ];
+            arr[ i + 1 ] = arr[ i ] + 1;
+            count += diff + 1;
+        }
+    } );
+    console.log( inputArray );
+    return count;
 }
 
-console.log(arrayChange([1, 1, 1]));
+console.log( arrayChange( [ 1, 1, 1 ] ) );
+console.log( arrayChange( [ 2, 1 ] ) );
