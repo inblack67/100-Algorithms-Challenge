d54ab6cf7b19386d64654931e19bc8840ba45eac
Aman Bhardwaj <inblack>
2021-02-21 20:23:59 +0530

    maximal adjacent sum

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,14 @@
-function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+const arrayMaximalAdjacentDifference = (inputArray: number[]): number => {
+    let res = Number.MIN_SAFE_INTEGER;
+    for (let i = 1; i < inputArray.length; i++) {
+        const pureDiff = inputArray[ i ] - inputArray[ i - 1 ];
+        const absDiff = Math.abs(pureDiff);
+        if (absDiff > res) {
+            res = absDiff;
+        }
+    }
+    return res;
+};
 
-}
-
-console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+const arr = [ 2, 4, 1, 0 ];
+console.log(arrayMaximalAdjacentDifference(arr));
