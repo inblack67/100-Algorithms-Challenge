bc17223e95d6482e7c8e844a8e8dfe3b3b3bd88b
Aman Bhardwaj <inblack>
2021-02-22 15:26:07 +0530

    tough

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,24 @@
-function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+function arrayMaxConsecutiveSum (inputArray: number[], k: number): number {
+    let sum = 0;
+    let max = 0;
 
+    for (let i = 0; i < k; i++) {
+        const el = inputArray[ i ];
+        sum += el;
+    }
+
+    max = sum;
+
+    for (let i = k; i < inputArray.length; i++) {
+        const el = inputArray[ i ];
+        sum -= inputArray[ i - k ];
+        sum += el;
+        if (sum > max) {
+            max = sum;
+        }
+    }
+
+    return max;
 }
 
-console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+console.log(arrayMaxConsecutiveSum([ 2, 7, 2, 5, 1, 6, 4 ], 2));
