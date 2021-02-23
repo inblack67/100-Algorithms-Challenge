09e5fc356967639d7f7079b09914a2089c784a07
Aman Bhardwaj <inblack>
2021-02-23 14:30:13 +0530

    avoidObstacles

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,31 @@
-function avoidObstacles(inputArray: number[]): number {
+const avoidObstacles = (inputArray: number[]): number => {
+    const copiedArray = [ ...inputArray ];
+    const sortedArr = copiedArray.sort((a, b) => a - b);
+    const arrLength = sortedArr[ sortedArr.length - 1 ];
+    const myPath = new Array(arrLength);
 
-}
+    for (let i = 0; i < arrLength; i++) {
+        myPath[ i ] = 0;
+    }
 
-console.log(avoidObstacles([5, 3, 6, 7, 9]));
+    sortedArr.forEach((el, i) => {
+        // -1 = an obstacle
+        myPath[ el ] = -1;     // populating obstacles
+    });
+
+    // console.log(myPath);
+
+    let jumps = -1;
+
+    myPath.forEach((el, i) => {
+        if (el !== -1) {
+            // safe
+            jumps++;
+        }
+    });
+
+    return jumps;
+};
+
+console.log(avoidObstacles([ 5, 3, 2, 7, 10, 15 ]));
+console.log(avoidObstacles([ 5, 3, 6, 7, 9 ]));
