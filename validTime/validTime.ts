abbe557c54eb8acdb5107a257fad8a1a2c2856b7
Aman Bhardwaj <inblack>
2021-02-23 18:53:33 +0530

    valid time

--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,7 +1,17 @@
-function validTime(time: string): boolean {
-
+function validTime (time: string): boolean {
+    const timeArr = time.split(':');
+    const hour = +timeArr[ 0 ];
+    const min = +timeArr[ 1 ];
+    if (hour > 23 || hour < 0) {
+        return false;
+    }
+    if (min > 59 || min < 0) {
+        return false;
+    }
+    return true;
 }
 
 console.log(validTime('13:58'));
 console.log(validTime('25:51'));
 console.log(validTime('02:76'));
+console.log(validTime('00:00'));
