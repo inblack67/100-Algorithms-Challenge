6191c65b39131983d3a84fd147c91bce44909567
Aman Bhardwaj <inblack>
2021-02-23 18:45:55 +0530

    bishop and the pawn

--- bishopAndPawn/bishopAndPawn.ts
+++ bishopAndPawn/bishopAndPawn.ts
@@ -1,5 +1,34 @@
-function bishopAndPawn(bishop: string, pawn: string): boolean {
+const bishopAndPawn = (bishop: string, pawn: string): boolean => {
 
-}
+    let result = false;
+
+    const chess = {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: 4,
+        e: 5,
+        f: 6,
+        g: 7,
+        h: 8,
+    };
+
+    const bishopX = chess[ bishop[ 0 ] ];
+    const bishopY = parseInt(bishop[ 1 ]);
+
+    const pawnX = chess[ pawn[ 0 ] ];
+    const pawnY = parseInt(pawn[ 1 ]);
+
+    const bishopCoordinates = { x: bishopX, y: bishopY };
+    const pawnCoordinates = { x: pawnX, y: pawnY };
+    console.log(bishopCoordinates);
+    console.log(pawnCoordinates);
+
+    if (bishopX + bishopY === pawnX + pawnY || bishopX + pawnY === bishopY + pawnX) {
+        result = true;
+    }
+
+    return result;
+};
 
 console.log(bishopAndPawn('a1', 'c3'));
