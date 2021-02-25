8202cc8f0c1797a4c64b9bf454e93229f1182359
Aman Bhardwaj <inblack>
2021-02-25 09:37:38 +0530

    better approach

--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,16 +1,31 @@
 const sumOfTwo = (a: number[], b: number[], v: number): boolean => {
-    for (let i = 0; i < a.length; i++) {
-        const el = a[ i ];
-        for (let j = 0; j < b.length; j++) {
-            const el2 = b[ j ];
-            const sum = el + el2;
-            if (sum === v) {
-                return true;
-            }
+
+    const hash = {};
+
+    for (const el of a) {
+        const diff = v - el;
+        hash[ diff ] = diff;
+    }
+
+    for (const el2 of b) {
+        if (hash.hasOwnProperty(el2)) {
+            return true;
         }
     }
 
-    return false;
+    // O(n^2)
+    // for (let i = 0; i < a.length; i++) {
+    //     const el = a[ i ];
+    //     for (let j = 0; j < b.length; j++) {
+    //         const el2 = b[ j ];
+    //         const sum = el + el2;
+    //         if (sum === v) {
+    //             return true;
+    //         }
+    //     }
+    // }
+
+    // return false;
 };
 
 console.log(sumOfTwo([ 1, 2, 3 ], [ 10, 20, 30, 40 ], 42));
