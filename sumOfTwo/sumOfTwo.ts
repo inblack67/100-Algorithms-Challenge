fc3aa9db29184762940dd1412caf9c0877ee7230
Aman Bhardwaj <inblack>
2021-02-25 09:30:05 +0530

    sumOfTwo => O(n^2)

--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,5 +1,16 @@
-function sumOfTwo(a: number[], b: number[], v: number): boolean {
+const sumOfTwo = (a: number[], b: number[], v: number): boolean => {
+    for (let i = 0; i < a.length; i++) {
+        const el = a[ i ];
+        for (let j = 0; j < b.length; j++) {
+            const el2 = b[ j ];
+            const sum = el + el2;
+            if (sum === v) {
+                return true;
+            }
+        }
+    }
 
-}
+    return false;
+};
 
-console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
+console.log(sumOfTwo([ 1, 2, 3 ], [ 10, 20, 30, 40 ], 42));
