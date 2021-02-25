1a063b76558048c6714599d5b23c63acaf762484
Aman Bhardwaj <inblack>
2021-02-25 09:24:12 +0530

    taskTypes

--- tasksType/tasksType.ts
+++ tasksType/tasksType.ts
@@ -1,6 +1,29 @@
-function tasksTypes(deadlines: number[], day: number): number[] {
-
+interface Deadlines {
+    today: number;
+    upcoming: number;
+    later: number;
 }
 
-console.log(tasksTypes([1, 2, 3, 4, 5], 2));
-console.log(tasksTypes([1, 2, 4, 2, 10, 3, 1, 4, 5, 4, 9, 8], 1));
+const tasksTypes = (deadlines: number[], today: number): number[] => {
+
+    const res: Deadlines = {
+        later: 0,
+        today: 0,
+        upcoming: 0
+    };
+
+    deadlines.forEach(el => {
+        if (el <= today) {
+            res.today++;
+        } else if (el > today && el <= today + 7) {
+            res.upcoming++;
+        } else {
+            res.later++;
+        }
+    });
+
+    return [ res.today, res.upcoming, res.later ];
+};
+
+console.log(tasksTypes([ 1, 2, 3, 4, 5 ], 2));
+console.log(tasksTypes([ 1, 2, 4, 2, 10, 3, 1, 4, 5, 4, 9, 8 ], 1));
