af079533150fc2fa5f79d04f90217c18d18b0b78
Aman Bhardwaj <inblack>
2021-02-25 10:44:38 +0530

    sumOddFibonacciNums

--- sumOddFibonacciNums/sumOddFibonacciNums.ts
+++ sumOddFibonacciNums/sumOddFibonacciNums.ts
@@ -1,7 +1,36 @@
-function sumOddFibonacciNums(num: number): number {
+const getFibonacci = (upTo: number): number[] => {
+    if (upTo <= 1) {
+        return [ 1 ];
+    }
+    const uptoArr = [];
+    for (let i = 0; i < upTo; i++) {
+        uptoArr[ i ] = i;
+    }
 
-}
 
+    const fib = [ 1, 1 ];
+
+    for (let i = 0; i < uptoArr.length; i++) {
+        const el = uptoArr[ i ];
+        if (el > 1) {
+            const sum = fib[ i - 2 ] + fib[ i - 1 ];
+            fib.push(sum);
+        }
+    }
+    return fib;
+};
+
+const sumOddFibonacciNums = (num: number): number => {
+    let sum = 0;
+    const fib = getFibonacci(num);
+    fib.forEach((el) => {
+        if (el % 2 !== 0 && el < num) {
+            sum += el;
+        }
+    });
+    return sum;
+};
+
+console.log(sumOddFibonacciNums(10));
 console.log(sumOddFibonacciNums(1000));
 console.log(sumOddFibonacciNums(4000000));
-console.log(sumOddFibonacciNums(10));
