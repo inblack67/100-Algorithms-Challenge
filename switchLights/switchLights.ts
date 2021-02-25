9e4eecf4d75c6f3e6448c6deb8c5f23111812918
Aman Bhardwaj <inblack>
2021-02-25 22:05:44 +0530

    switchLights

--- switchLights/switchLights.ts
+++ switchLights/switchLights.ts
@@ -1,7 +1,40 @@
-function switchLights(a: number[]): number[] {
+const swapDigits = (arr: number[]): number[] => {
+    const res = arr.map((el) => el === 0 ? 1 : 0);
+    return res;
+};
 
+function switchLights (a: number[]): number[] {
+
+    let arr = [ ...a ];
+
+    arr.forEach((light, i) => {
+        if (light === 1) {
+            if (i === 0) {
+                arr[ 0 ] = 0;
+            } else if (i === 1) {
+                arr[ 0 ] = arr[ 0 ] === 1 ? 0 : 1;
+                arr[ 1 ] = 0;
+            } else {
+                const prevLights = arr.slice(0, i);
+                const swappedPrevDigits = swapDigits(prevLights);
+                swappedPrevDigits.forEach((el, j) => {
+                    arr[ j ] = el;
+                });
+                arr[ i ] = 0;
+            }
+        }
+    });
+
+    return arr;
 }
 
+const arr = [ 1, 2, 3, 4 ];
+const sliceMe = (arr: number[]) => arr.slice(2, 4);
+console.log(sliceMe(arr));
 
-console.log(switchLights([1, 1, 1, 1, 1]));
-console.log(switchLights([0, 0]));
+console.log(switchLights([ 1, 1, 1, 1, 1 ]));
+console.log(switchLights([ 1, 1, 1, 1, 1 ]));
+console.log(switchLights([ 1, 1, 1, 1, 1 ]));
+console.log(switchLights([ 1, 1, 1, 1, 1 ]));
+console.log(switchLights([ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ]));
+console.log(switchLights([ 0, 0 ]));
