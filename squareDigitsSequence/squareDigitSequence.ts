9c401743abe78fd61966933d7d0886d3852da3cf
Aman Bhardwaj <inblack>
2021-02-26 11:33:14 +0530

    squareDigitsSequence

--- squareDigitsSequence/squareDigitSequence.ts
+++ squareDigitsSequence/squareDigitSequence.ts
@@ -1,6 +1,32 @@
-function squareDigitsSequence(a0: number): number {
+const getSumOfSquares = (num: number) => {
+    let sum = 0;
+    const strArr = num.toString().split('');
+    strArr.forEach(el => {
+        sum += +el * +el;
+    });
+    return sum;
+};
 
-}
+const squareDigitsSequence = (a0: number): number => {
+    let count = 1;
+    let sum = 0;
+    const res: number[] = [ a0 ];
+
+    let temp = a0;
+
+    do {
+        sum = getSumOfSquares(temp);
+        if (res.includes(sum)) {
+            count++;
+            break;
+        }
+        res.push(sum);
+        temp = sum;
+        count++;
+    } while (true);
+
+    return count;
+};
 
 
 console.log(squareDigitsSequence(16));
