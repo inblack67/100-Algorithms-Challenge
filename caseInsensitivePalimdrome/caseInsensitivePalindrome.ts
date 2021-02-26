16fbbdec6787424ca668c8f9382063b6751ad008
Aman Bhardwaj <inblack>
2021-02-26 11:50:11 +0530

    isCaseInsensitivePalindrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,8 @@
-function isCaseInsensitivePalindrome(inputString: string): boolean {
-
-}
+const isCaseInsensitivePalindrome = (inputString: string): boolean => {
+    const pureStrArr = inputString.toLowerCase().split('');
+    const reversedPureStrArr = [ ...pureStrArr ].reverse();
+    return pureStrArr.join('') === reversedPureStrArr.join('');
+};
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
 console.log(isCaseInsensitivePalindrome('abac'));
