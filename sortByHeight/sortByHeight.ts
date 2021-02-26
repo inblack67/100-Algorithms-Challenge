7ecf66d583d93b47a6dd5ff643c0af2cfbd751ca
Aman Bhardwaj <inblack>
2021-02-26 22:43:32 +0530

    sortByHeight

--- sortByHeight/sortByHeight.ts
+++ sortByHeight/sortByHeight.ts
@@ -1,5 +1,15 @@
-function sortByHeight(a: number[]): number[] {
+const sortByHeight = (a: number[]): number[] => {
+    const arr = [ ...a ];
+    const filteredArr = arr.filter(el => el !== -1);
+    const sortedFilterArr = filteredArr.sort((a, b) => a - b);
+    let j = 0;
+    arr.forEach((el, i) => {
+        if (el !== -1) {
+            arr[ i ] = sortedFilterArr[ j ];
+            j++;
+        }
+    });
+    return arr;
+};
 
-}
-
-console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
+console.log(sortByHeight([ -1, 150, 190, 170, -1, -1, 160, 180 ]));
