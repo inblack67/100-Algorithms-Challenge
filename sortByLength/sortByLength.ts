68e5e8d42a03d0f3771b5ec8bb9aa0c2582dc215
Aman Bhardwaj <inblack>
2021-02-26 21:28:59 +0530

    sortByLength cool approach

--- sortByLength/sortByLength.ts
+++ sortByLength/sortByLength.ts
@@ -1,12 +1,15 @@
 const sortByLength = (inputArray: string[]): string[] => {
     const copiedArray = [ ...inputArray ];
-    copiedArray.sort((a, b) => {
-        if (a.length >= b.length) {
-            return 1;
-        } else {
-            return -1;
-        }
-    });
+    copiedArray.sort((a, b) => a.length - b.length);
+
+    // heavy
+    // copiedArray.sort((a, b) => {
+    //     if (a.length >= b.length) {
+    //         return 1;
+    //     } else {
+    //         return -1;
+    //     }
+    // });
 
     return copiedArray;
 };
