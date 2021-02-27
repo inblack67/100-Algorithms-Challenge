6ad19c54c1ab0243570194a145fae9801ca3856c
Aman Bhardwaj <inblack>
2021-02-27 12:25:08 +0530

    reflectString better code

--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -1,6 +1,24 @@
 const reflectString = (inputString: string): string => {
-    const strArr = inputString.split('');
+    const dictionary = {};
+    for (let i = 97; i <= 122; i++) {
+        const char = String.fromCharCode(i);
+        dictionary[ char ] = String.fromCharCode(i - 1);
+    }
+    dictionary[ 'a' ] = 'z';
+    dictionary[ 'z' ] = 'a';
 
+    const str = inputString.toLowerCase();
+    const strArr = str.split('');
+    strArr.forEach((el, i) => {
+        strArr[ i ] = dictionary[ el ];
+    });
+    const res = strArr.join('');
+    return res;
+};
+
+const reflectString2 = (inputString: string): string => {
+    const str = inputString.toLowerCase();
+    const strArr = str.split('');
     strArr.forEach((el, i) => {
         const code = el.charCodeAt(0);
         if (code === 97) {
