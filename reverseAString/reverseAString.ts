3fe8cd972f0c5ac4637b0363331231de4ea828d3
Aman Bhardwaj <inblack>
2021-02-27 11:40:30 +0530

    reverse a string

--- reverseAString/reverseAString.ts
+++ reverseAString/reverseAString.ts
@@ -1,6 +1,4 @@
-function reverseAString(str: string): string {
-
-}
+const reverseAString = (str: string): string => str.split('').reverse().join('');
 
 console.log(reverseAString('hello'));
 console.log(reverseAString('Howdy'));
