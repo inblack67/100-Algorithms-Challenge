ddd66da2b954425661cf56f9c3ab407ca16127e1
Aman Bhardwaj <inblack>
2021-02-27 11:52:17 +0530

    string reversals

--- reverseAString/reverseAString.ts
+++ reverseAString/reverseAString.ts
@@ -1,4 +1,13 @@
 const reverseAString = (str: string): string => str.split('').reverse().join('');
 
+const reverseAString2 = (str: string): string => {
+    let revString = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        const el = str[ i ];
+        revString += el;
+    }
+    return revString;
+};
+
 console.log(reverseAString('hello'));
 console.log(reverseAString('Howdy'));
