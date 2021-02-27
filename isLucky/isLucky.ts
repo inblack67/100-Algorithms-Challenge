73b79b81d441e6b7b569ee7eb4e1ebf69ba2a9ca
Aman Bhardwaj <inblack>
2021-02-27 23:54:50 +0530

    is lucky

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,6 +1,20 @@
-function isLucky(n: number): boolean {
-    
-}
+const isLucky = (n: number): boolean => {
+    const str = n.toString();
+    const strLength = str.length;
+    const part1 = str.slice(0, strLength / 2);
+    const part2 = str.slice(strLength / 2, strLength);
+    const part1Arr = part1.split('');
+    const part2Arr = part2.split('');
+    let sum1 = 0;
+    let sum2 = 0;
+    part1Arr.forEach((el) => {
+        sum1 += +el;
+    });
+    part2Arr.forEach(el => {
+        sum2 += +el;
+    });
+    return sum1 === sum2;
+};
 
 console.log(isLucky(1230));
 console.log(isLucky(239017));
