e3ab79619cbdaccad3d6fa473a14e82e7bec5129
Aman Bhardwaj <inblack>
2021-02-27 23:47:24 +0530

    largestNumber better code

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,9 +1,13 @@
-const largestNumber = (n: number): number => {
+const largestNumber2 = (n: number): number => {
     let res = '';
     for (let i = 0; i < n; i++) {
         res += 9;
     }
     return +res;
 };
+const largestNumber = (n: number): number => {
+    const str = '9'.repeat(n);
+    return +str;
+};
 
-console.log(largestNumber(2));
+console.log(largestNumber(9));
