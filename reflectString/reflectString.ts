3fdfeb97c7881d8aac6f4e5d844a0730d02ab648
Aman Bhardwaj <inblack>
2021-02-27 12:12:01 +0530

    reflectString

--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -1,5 +1,19 @@
-function reflectString(inputString: string): string {
-  
-}
+const reflectString = (inputString: string): string => {
+    const strArr = inputString.split('');
 
-console.log(reflectString("name"));
+    strArr.forEach((el, i) => {
+        const code = el.charCodeAt(0);
+        if (code === 97) {
+            strArr[ i ] = 'z';
+        } else if (code === 122) {
+            strArr[ i ] = 'a';
+        }
+        else {
+            strArr[ i ] = String.fromCharCode(code - 1);
+        }
+    });
+
+    return strArr.join('');
+};
+
+console.log(reflectString("namez"));
