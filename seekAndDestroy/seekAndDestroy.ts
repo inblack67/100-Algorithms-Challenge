37b3480bb084203cd8e33d169c33658d23ad6947
Aman Bhardwaj <inblack>
2021-02-27 11:59:06 +0530

    seekAndDestroy

--- seekAndDestroy/seekAndDestroy.ts
+++ seekAndDestroy/seekAndDestroy.ts
@@ -1,6 +1,14 @@
-function seekAndDestroy(arr1: number[], arr2: number[]): number[] {
-   
-}
+const seekAndDestroy = (arr1: number[], arr2: number[]): number[] => arr1.filter(el => !arr2.includes(el));
 
-console.log(seekAndDestroy([3, 5, 1, 2, 2], [2, 3, 5]));
-console.log(seekAndDestroy([1, 2, 3, 5, 1, 2, 3], [2, 3]));
+const seekAndDestroy2 = (arr1: number[], arr2: number[]): number[] => {
+    const res = [];
+    arr1.forEach((el) => {
+        if (!arr2.includes(el)) {
+            res.push(el);
+        };
+    });
+    return res;
+};
+
+console.log(seekAndDestroy([ 3, 5, 1, 2, 2 ], [ 2, 3, 5 ]));
+console.log(seekAndDestroy([ 1, 2, 3, 5, 1, 2, 3 ], [ 2, 3 ]));
