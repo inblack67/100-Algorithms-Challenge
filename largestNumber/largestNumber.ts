483d553d0caa4a38cdea17c240ea9723fa94cc58
Aman Bhardwaj <inblack>
2021-02-27 23:43:14 +0530

    largestNumber

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,9 @@
-function largestNumber(n: number): number {
- 
-}
+const largestNumber = (n: number): number => {
+    let res = '';
+    for (let i = 0; i < n; i++) {
+        res += 9;
+    }
+    return +res;
+};
 
 console.log(largestNumber(2));
