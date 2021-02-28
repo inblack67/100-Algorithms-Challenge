a368645bb9cf5093e7d15cbe970dacd2b98839cf
Aman Bhardwaj <inblack>
2021-02-28 10:03:17 +0530

    checkPalindrome

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,5 +1,9 @@
-function checkPalindrome(inputString: string): boolean {
-
+function checkPalindrome (inputString: string): boolean {
+    const str = inputString.trim().toLowerCase();
+    const strArr = str.split('');
+    const s1 = strArr.join('');
+    const s2 = [ ...strArr ].reverse().join('');
+    return s1 === s2;
 }
 
 console.log(checkPalindrome('aabaa'));
