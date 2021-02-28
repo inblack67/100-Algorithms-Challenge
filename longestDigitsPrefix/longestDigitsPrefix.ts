ba85a633689f7b3f0033883633f4df468a7fb3d8
Aman Bhardwaj <inblack>
2021-02-28 09:55:45 +0530

    longestDigitsPrefix

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,32 @@
-function longestDigitsPrefix(inputString: string): string {
+const isAlpha = (ch: string) => ch.match(/[a-z]/gi);
 
-}
+const longestDigitsPrefix2 = (inputString: string): string => {
+    const strArr = inputString.split('');
+    const res = [];
+    for (let i = 0; i < strArr.length; i++) {
+        const el = strArr[ i ];
+        if (!isAlpha(el)) {
+            res.push(el);
+        } else {
+            break;
+        }
+    }
+    return res.join('');
+};
+
+const longestDigitsPrefix = (inputString: string): string => {
+    const strArr = inputString.split('');
+    let res = '';
+    for (let i = 0; i < strArr.length; i++) {
+        const el = strArr[ i ];
+        if (isNaN(+el)) {
+            break;
+        } else {
+            res += el;
+        }
+    }
+    return res;
+};
+
 
 console.log(longestDigitsPrefix('123aa1'));
