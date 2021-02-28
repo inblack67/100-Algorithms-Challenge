65f0173358d5b6f1afb1fd8415f796375021748d
Aman Bhardwaj <inblack>
2021-02-28 11:15:34 +0530

    characterParity

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,18 @@
-function characterParity(symbol: string): string {
-   
+function characterParity (symbol: string): string {
+    const parsedSymbol = +symbol;
+
+    return isNaN(parsedSymbol) ? 'not a digit' : (parsedSymbol % 2 === 0 ? 'even' : 'odd');
+
+    // somewhat costly
+    // if (isNaN(parsedSymbol)) {
+    //     return 'not a digit';
+    // } else if (parsedSymbol % 2 === 0) {
+    //     return 'even';
+    // } else {
+    //     return 'odd';
+    // }
 }
 
-console.log(characterParity('5'))
-console.log(characterParity('8'))
-console.log(characterParity('q'))
+console.log(characterParity('5'));
+console.log(characterParity('8'));
+console.log(characterParity('q'));
