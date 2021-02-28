a2ae6f384d099e74174dd772b4a5d1cf8991861f
Aman Bhardwaj <inblack>
2021-02-28 09:58:16 +0530

    properNounCorrection

--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,6 +1,8 @@
-function properNounCorrection(noun: string): string {
-   
-}
+const properNounCorrection = (noun: string): string => {
+    const str = noun.trim().toLowerCase();
+    const res = str[ 0 ].toUpperCase() + str.slice(1);
+    return res;
+};
 
 console.log(properNounCorrection('pARiS'));
 console.log(properNounCorrection('John'));
