66af21991a02f3500ca4571118ee7e45d5e2c243
Aman Bhardwaj <inblack>
2021-02-28 15:25:04 +0530

    circleOfNumbers

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,17 @@
-function circleOfNumbers(n: number, firstNumber: number): number {
-
+function circleOfNumbers (n: number, firstNumber: number): number {
+    const clock: number[] = [];
+    for (let i = 0; i < n; i++) {
+        clock.push(i);
+    }
+    const mid = n / 2;
+    let target: number;
+    if (firstNumber >= mid) {
+        target = firstNumber - mid;
+    } else {
+        target = clock.indexOf(firstNumber) + mid;
+    }
+    return clock[ target ];
 }
 
 console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 9));
