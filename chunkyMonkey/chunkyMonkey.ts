6010f6c83df6a64cefc0080384fe76660ee60b1d
Aman Bhardwaj <inblack>
2021-02-28 18:08:39 +0530

    chunkyMonkey

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,13 @@
-function chunkyMonkey(arr: any[], size: number): any[][] {
-
+function chunkyMonkey (arr: any[], size: number): any[][] {
+    console.log(arr);
+    console.log(size);
+    const part1 = arr.slice(0, size);   // second arg of slice is exlusive
+    const part2 = arr.slice(size);
+    const res: any[][] = [];
+    res.push(part1);
+    res.push(part2);
+    return res;
 }
 
-console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
-console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkyMonkey([ "a", "b", "c", "d" ], 2));
+console.log(chunkyMonkey([ 0, 1, 2, 3, 4, 5 ], 4));
