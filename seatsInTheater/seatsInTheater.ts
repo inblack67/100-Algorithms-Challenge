d5fccf94fc50ce89d2ead733c872d3c4215fdd98
Aman Bhardwaj <inblack>
2021-02-28 12:16:50 +0530

    seatsInTheater

--- seatsInTheater/seatsInTheater.ts
+++ seatsInTheater/seatsInTheater.ts
@@ -1,5 +1,8 @@
-function seatsInTheater(nCols: number, nRows: number, col: number, row: number): number {
-
+function seatsInTheater (nCols: number, nRows: number, col: number, row: number): number {
+    const tRow = nRows - row;
+    const tCol = nCols - col + 1;   // my col will be included too
+    const newOrder = tRow * tCol;
+    return newOrder;
 }
 
 console.log(seatsInTheater(16, 11, 5, 3));
