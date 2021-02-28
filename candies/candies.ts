bb9c1ab855b3af8f7aca4da1e68d291ae70689b2
Aman Bhardwaj <inblack>
2021-02-28 11:49:43 +0530

    candies

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,9 @@
-function candies(n: number, m: number): number {
-
+function candies (n: number, m: number): number {
+    // n => children
+    // m => candies
+    const minCandies = Math.floor(m / n);
+    const totalCandies = minCandies * n;
+    return totalCandies;
 }
 
 console.log(candies(3, 10));
