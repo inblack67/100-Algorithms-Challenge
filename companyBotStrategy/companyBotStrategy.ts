a8623aa8ac8f933eab98d1e49913a98d540c3eb0
Aman Bhardwaj <inblack>
2021-03-01 10:34:17 +0530

    companyBotStrategy

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,7 +1,20 @@
-function companyBotStrategy(trainingData: number[][]): number {
- 
+function companyBotStrategy (trainingData: number[][]): number {
+    const data = [ ...trainingData ];
+    let sum = 0;
+    let count = 0;
+
+    data.forEach((arr) => {
+        if (arr[ 1 ] === 1) {
+            sum += arr[ 0 ];
+            count++;
+        }
+    });
+
+    const avg = sum ? sum / count : 0;
+    return avg;
+
 }
 
-console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
-console.log(companyBotStrategy([[4, 1], [4, -1], [0, 0], [6, 1]]));
-console.log(companyBotStrategy( [[4, -1], [0, 0], [5, -1]]));
+console.log(companyBotStrategy([ [ 3, 1 ], [ 6, 1 ], [ 4, 1 ], [ 5, 1 ] ]));
+console.log(companyBotStrategy([ [ 4, 1 ], [ 4, -1 ], [ 0, 0 ], [ 6, 1 ] ]));
+console.log(companyBotStrategy([ [ 4, -1 ], [ 0, 0 ], [ 5, -1 ] ]));
