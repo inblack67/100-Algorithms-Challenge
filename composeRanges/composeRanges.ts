71a83685d2515b888a8d45a87d1f0e024c6253e6
Aman Bhardwaj <inblack>
2021-03-01 12:06:37 +0530

    confirmEnding

--- composeRanges/composeRanges.ts
+++ composeRanges/composeRanges.ts
@@ -1,6 +1,27 @@
-function composeRanges(nums: number[]): string[] {
+type MyRange = {
+    start?: number;
+    end?: number;
+};
 
+function composeRanges (nums: number[]): string[] {
+    console.log(nums);
+    let targetIndex = 0;
+    let obj: MyRange = {};
+    let tryMe = 0;
+    nums.forEach((el, i) => {
+        const nextEl = nums[ i + 1 ];
+        const diff = Math.abs(el - nextEl);
+        if (diff === 1) {
+            tryMe = 1;
+            targetIndex++;
+        } else {
+            tryMe = 0;
+        }
+        console.log(tryMe);
+    });
+    console.log(obj);
+    return null;
 }
 
 
-console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
+console.log(composeRanges([ -1, 0, 1, 2, 6, 7, 9 ]));
--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,10 @@
-function confirmEnding(str: string, target: string) {
-   
-}
+const confirmEnding2 = (str: string, target: string) => str.endsWith(target);
+
+const confirmEnding = (str: string, target: string) => {
+    const sub = str.substring(str.length - target.length);
+    return sub === target;
+};
 
 console.log(confirmEnding("Abstraction", "action"));
 console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding("yo whats 7up", " 7up"));
