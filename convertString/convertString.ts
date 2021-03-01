249b34d26a8dbae57a79681f9d8cf9a81166bbc0
Aman Bhardwaj <inblack>
2021-03-01 19:02:49 +0530

    convertString

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,6 +1,39 @@
-function convertString(s: string, t: string): boolean {
-
+function convertString (s: string, t: string): boolean {
+    const sourceArr = s.split('');
+    const targetArr = t.split('');
+    const sourceCounts = {};
+    const targetCounts = {};
+    sourceArr.forEach(el => {
+        if (sourceCounts[ el ]) {
+            sourceCounts[ el ]++;
+        } else {
+            sourceCounts[ el ] = 1;
+        }
+    });
+    targetArr.forEach(el => {
+        if (targetCounts[ el ]) {
+            targetCounts[ el ]++;
+        } else {
+            targetCounts[ el ] = 1;
+        }
+    });
+    for (const key in targetCounts) {
+        if (Object.prototype.hasOwnProperty.call(targetCounts, key)) {
+            if (!sourceCounts[ key ]) {
+                return false;
+            }
+        }
+    }
+    for (const key in sourceCounts) {
+        if (Object.prototype.hasOwnProperty.call(sourceCounts, key)) {
+            const el = sourceCounts[ key ];
+            if (targetCounts[ key ] > el) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
-console.log(convertString('addbyca', 'abcd'));
+console.log(convertString('addbyca', 'abcddy'));
