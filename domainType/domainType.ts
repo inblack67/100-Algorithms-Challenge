daf107cd3e88b7709e9e084641f87b64c73f665b
Aman Bhardwaj <inblack>
2021-03-02 00:45:56 +0530

    perfect code

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,32 +1,22 @@
 function domainType (domains: string[]): string[] {
 
+    const domainObj = {
+        org: 'organization',
+        com: 'commercial',
+        net: 'network',
+        info: 'information'
+    };
+
     const res: string[] = [];
 
     domains.forEach(domain => {
         const domainArr = domain.split('.');
-        console.log(domainArr);
         const topLevel = domainArr[ domainArr.length - 1 ];
         ;
-        console.log(topLevel);
-        res.push(topLevel);
+        res.push(domainObj[ topLevel ]);
     });
 
-    const final = res.map((el) => {
-        switch (el) {
-            case 'org':
-                return 'organization';
-            case 'com':
-                return 'commercial';
-            case 'net':
-                return 'network';
-            case 'info':
-                return 'information';
-            default:
-                return el;
-        }
-    });
-
-    return final;
+    return res;
 }
 
 console.log(domainType([ "en.wiki.org", "codefights.com", "happy.net", "code.info" ]));
