092157564ee974c1deb310874747e8e91c586990
Aman Bhardwaj <inblack>
2021-03-02 00:41:03 +0530

    domainType

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,32 @@
-function domainType(domains: string[]): string[] {
+function domainType (domains: string[]): string[] {
 
+    const res: string[] = [];
+
+    domains.forEach(domain => {
+        const domainArr = domain.split('.');
+        console.log(domainArr);
+        const topLevel = domainArr[ domainArr.length - 1 ];
+        ;
+        console.log(topLevel);
+        res.push(topLevel);
+    });
+
+    const final = res.map((el) => {
+        switch (el) {
+            case 'org':
+                return 'organization';
+            case 'com':
+                return 'commercial';
+            case 'net':
+                return 'network';
+            case 'info':
+                return 'information';
+            default:
+                return el;
+        }
+    });
+
+    return final;
 }
 
-console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(domainType([ "en.wiki.org", "codefights.com", "happy.net", "code.info" ]));
