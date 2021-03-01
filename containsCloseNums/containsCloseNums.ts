f49b273e8487c99b4a52ca8fccf21e8fba664927
Aman Bhardwaj <inblack>
2021-03-01 12:24:44 +0530

    containsCloseNums

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,6 +1,20 @@
-function containsCloseNums(nums: number[], k: number): boolean {
+const containsCloseNums = (nums: number[], k: number): boolean => {
 
-}
+    for (let i = 0; i < nums.length; i++) {
+        const el = nums[ i ];
+        for (let j = 0; j < nums.length; j++) {
+            const el2 = nums[ j ];
+            if ((i !== j) && (el === el2)) {
+                const absSum = Math.abs(i - j);
+                if (absSum <= k) {
+                    return true;
+                }
+            }
+        }
+    }
 
-console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
-console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
+    return false;
+};
+
+console.log(containsCloseNums([ 0, 1, 2, 3, 5, 2 ], 3));
+console.log(containsCloseNums([ 0, 1, 2, 3, 5, 2 ], 2));
