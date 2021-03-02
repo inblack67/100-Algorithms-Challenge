7ae8a3d0bf47a54166130c7acccf57925eee86ac
Aman Bhardwaj <inblack>
2021-03-02 10:54:18 +0530

    factorializeANumber2

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -4,6 +4,20 @@ function factorializeANumber (num: number): number {
     }
     return num * factorializeANumber(num - 1);
 }
+function factorializeANumber2 (num: number): number {
+    if (num <= 1) {
+        return num;
+    }
+
+    let res = 1;
+
+    for (let i = 2; i <= num; i++) {
+        res *= i;
+    }
+    return res;
+}
 
 console.log(factorializeANumber(5));
 console.log(factorializeANumber(10));
+console.log(factorializeANumber2(5));
+console.log(factorializeANumber2(10));
