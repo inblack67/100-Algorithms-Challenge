78ff4b0753dbc2e4ecf2c0a36ebf7fe843650946
Aman Bhardwaj <inblack>
2021-03-02 10:46:53 +0530

    extractEachKth2

--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -16,4 +16,8 @@ function extractEachKth (inputArray: number[], k: number): number[] {
     return res;
 }
 
+const extractEachKth2 = (inputArray: number[], k: number): number[] => inputArray.filter((_, i) => (i + 1) % k !== 0);
+
+
 console.log(extractEachKth([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ], 3));
+console.log(extractEachKth2([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ], 3));
