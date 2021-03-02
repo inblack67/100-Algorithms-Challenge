5e7dd57c09e07aeecc44d5a8c1acd047cdd5b491
Aman Bhardwaj <inblack>
2021-03-02 21:57:08 +0530

    first digitn

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,5 +1,8 @@
-function firstDigit(inputString: string): string {
+const isNumeric = (char: string) => char.match(/[0-9]/gi);
 
+function firstDigit (inputString: string): string {
+    const nums = isNumeric(inputString);
+    return nums[ 0 ];
 }
 
 console.log(firstDigit('var_1__Int'));
