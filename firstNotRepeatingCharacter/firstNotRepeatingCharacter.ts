d02dbfa372f086c752859f9463dac8c519de3e24
Aman Bhardwaj <inblack>
2021-03-02 22:20:49 +0530

    firstNotRepeatingCharacter

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,5 +1,32 @@
-function firstNotRepeatingCharacter(s: string): string {
+function firstNotRepeatingCharacter2 (s: string): string {
+    const obj = {};
+    for (let i = 0; i < s.length; i++) {
+        const el = s[ i ];
+        if (obj[ el ]) {
+            obj[ el ]++;
+        } else {
+            obj[ el ] = 1;
+        }
+    }
+    for (const i in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, i)) {
+            const el = obj[ i ];
+            if (el === 1) {
+                return i;
+            }
+        }
+    }
 
+    return '_';
+}
+function firstNotRepeatingCharacter (s: string): string {
+    for (let i = 0; i < s.length; i++) {
+        const el = s[ i ];
+        if (s.indexOf(el) === s.lastIndexOf(el)) {
+            return el;
+        }
+    }
+    return '_';
 }
 
 console.log(firstNotRepeatingCharacter('abacabad'));
