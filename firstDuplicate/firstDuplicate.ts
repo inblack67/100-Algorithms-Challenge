fca31565ecd82e4d4d7de5d141856f54380e9767
Aman Bhardwaj <inblack>
2021-03-02 22:08:56 +0530

    firstDuplicate

--- firstDuplicate/firstDuplicate.ts
+++ firstDuplicate/firstDuplicate.ts
@@ -1,6 +1,14 @@
-function firstDuplicate(a: number[]): number {
-    
+function firstDuplicate (a: number[]): number {
+    const obj = {};
+    for (let i = 0; i < a.length; i++) {
+        const el = a[ i ];
+        if (obj[ el ]) {
+            return el;  // second occurence
+        }
+        obj[ el ] = el; // first occurence
+    }
+    return -1;
 }
 
-console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
-console.log(firstDuplicate([2, 4, 3, 5, 1]));
+console.log(firstDuplicate([ 2, 1, 3, 5, 3, 2 ]));
+console.log(firstDuplicate([ 2, 4, 3, 5, 1 ]));
