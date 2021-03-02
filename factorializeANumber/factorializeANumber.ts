206b8b144eac178daf8f863a3c2dc1bd590aa138
Aman Bhardwaj <inblack>
2021-03-02 10:49:20 +0530

    factorializeANumber

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,5 +1,8 @@
-function factorializeANumber(num: number): number {
-
+function factorializeANumber (num: number): number {
+    if (num <= 1) {
+        return num;
+    }
+    return num * factorializeANumber(num - 1);
 }
 
 console.log(factorializeANumber(5));
