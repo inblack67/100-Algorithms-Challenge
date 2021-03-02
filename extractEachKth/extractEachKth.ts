8bba7d3325e3ae5cfdbae7d1742ddf25787a0efa
Aman Bhardwaj <inblack>
2021-03-02 10:42:44 +0530

    extractEachKth

--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,4 +1,19 @@
-function extractEachKth(inputArray:number[], k: number): number[] {
+function extractEachKth (inputArray: number[], k: number): number[] {
+    const copy = [ ...inputArray ];
+    const res: number[] = [];
+    let count = 0;
+
+    for (let i = 0; i < copy.length; i++) {
+        const el = copy[ i ];
+        if (count !== k - 1) {
+            count++;
+            res.push(el);
+        } else {
+            count = 0;
+        }
+    }
+
+    return res;
 }
 
-console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+console.log(extractEachKth([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ], 3));
