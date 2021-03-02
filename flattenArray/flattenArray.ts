b0ee8a2e0b58cb22894691563e6ac17b3db7c52e
Aman Bhardwaj <inblack>
2021-03-02 22:33:44 +0530

    flattenArray

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,6 +1,15 @@
-function flattenArray(arr: any[]): any[] {
-
+const res = [];
+function flattenArray (arr: any[]): any[] {
+    for (let i = 0; i < arr.length; i++) {
+        const el = arr[ i ];
+        if (Array.isArray(el)) {
+            flattenArray(el);
+        } else {
+            res.push(el);
+        }
+    }
+    return res;
 }
 
-console.log(flattenArray([[["a"]], [["b"]]]));
-console.log(flattenArray([1, [2], [3, [[4]]]]));
+console.log(flattenArray([ [ [ "a" ] ], [ [ "b" ] ] ]));
+console.log(flattenArray([ 1, [ 2 ], [ 3, [ [ 4 ] ] ] ]));
