51d238ef785e2880c16354af743bea992423d8b8
Aman Bhardwaj <inblack>
2021-03-02 10:26:57 +0530

    enclose in brackets

--- encloseInBrackets/encloseInBrackets.ts
+++ encloseInBrackets/encloseInBrackets.ts
@@ -1,5 +1,13 @@
-function encloseInBrackets(inputString: string): string {
+function encloseInBrackets (inputString: string): string {
+    const strArr = inputString.split('');
+    strArr.unshift('(');
+    strArr.push(')');
+    return strArr.join('');
+}
 
+function encloseInBrackets2 (inputString: string): string {
+    return `(${ inputString })`;
 }
 
 console.log(encloseInBrackets('abacaba'));
+console.log(encloseInBrackets2('abacaba'));
