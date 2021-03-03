fb4cb7fcd817b490a7f3fbf979a905e86acae1d4
Aman Bhardwaj <inblack>
2021-03-03 10:47:17 +0530

    incorrectPasscodeAttempts

--- incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,14 @@
-function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
-
+function incorrectPasscodeAttempts (passcode: string, attempts: string[]): boolean {
+    let tried = 0;
+    for (let i = 0; i < attempts.length; i++) {
+        const el = attempts[ i ];
+        if (el === passcode && tried < 10) {
+            tried = 0;
+        } else {
+            tried++;
+        }
+    }
+    return tried >= 10;
 }
 
-console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+console.log(incorrectPasscodeAttempts('1111', [ "1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111" ]));
