bf179193ec3ee3914b633f8141a9aead737e5da5
Aman Bhardwaj <inblack>
2021-03-03 16:05:47 +0530

    lateRide

--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -1,5 +1,29 @@
-function lateRide(n: number): number {
+type Timer = {
+    hours: number;
+    minutes: number;
+};
 
+const getSumOfDigits = (num: number): number => {
+    const str = num.toString();
+    if (str.length <= 1) {
+        return num;
+    }
+    let sum = 0;
+    const strArr = str.split('');
+    strArr.forEach(el => sum += +el);
+    return sum;
+};
+
+function lateRide (n: number): number {
+    const timer: Timer = { hours: 0, minutes: 0 };
+    const hours = Math.floor(n / 60);
+    const minutes = n % 60;
+    timer.hours = hours;
+    timer.minutes = minutes;
+    const sumOfHoursDigits = getSumOfDigits(timer.hours);
+    const sumOfMinutesDigits = getSumOfDigits(timer.minutes);
+    const res = sumOfHoursDigits + sumOfMinutesDigits;
+    return res;
 }
 
 console.log(lateRide(240));
