11cd52da43e689dfe3dfd8a7a59663f1f42bb605
Aman Bhardwaj <inblack>
2021-03-03 17:41:24 +0530

    depositProfit

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,11 @@
-function depositProfit(deposit: number, rate: number, threshold: number): number {
- 
+function depositProfit (deposit: number, rate: number, threshold: number): number {
+    let totalAmount = deposit;
+    let years = 0;
+    while (totalAmount <= threshold) {
+        totalAmount = totalAmount + ((rate / 100) * totalAmount);
+        years++;
+    }
+    return years;
 }
 
-console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170));
