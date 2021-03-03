637ace3d4451a201ebba735938967ea4d4626c48
Aman Bhardwaj <inblack>
2021-03-03 20:09:38 +0530

    maxMultiple2

--- maxMultiple/maxMultiple.ts
+++ maxMultiple/maxMultiple.ts
@@ -1,9 +1,13 @@
-function maxMultiple (divisor: number, bound: number): number {
+function maxMultiple2 (divisor: number, bound: number): number {
     for (let i = bound; i > 0; i--) {
         if (i % divisor === 0) {
             return i;
         }
     }
+}
+function maxMultiple (divisor: number, bound: number): number {
+    const rem = bound % divisor;
+    return bound - rem;
 }
 
 console.log(maxMultiple(3, 10));
