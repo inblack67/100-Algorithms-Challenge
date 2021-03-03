86166b3ffe75f01dca7920c08731cb19fe88657d
Aman Bhardwaj <inblack>
2021-03-03 13:24:05 +0530

    isTandemRepeat

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,10 @@
-function isTandemRepeat(inputString: string): boolean {
-  
+function isTandemRepeat (inputString: string): boolean {
+    const strLen = inputString.length;
+    const half1 = inputString.slice(0, strLen / 2);
+    const half2 = inputString.slice(strLen / 2);
+    return half1 === half2;
 }
 
-console.log(isTandemRepeat('tandemtandem'))
-console.log(isTandemRepeat('qqq'))
-console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat('tandemtandem'));
+console.log(isTandemRepeat('qqq'));
+console.log(isTandemRepeat('2w2ww'));
