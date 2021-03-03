936eb21ab69656d8b5264c6b2fb0ecdd32d2cc3f
Aman Bhardwaj <inblack>
2021-03-03 13:12:18 +0530

    integerToStringOfFixedWidth

--- integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
@@ -1,5 +1,26 @@
-function integerToStringOfFixedWidth(number: number, width: number): string {
-  
+function integerToStringOfFixedWidth (number: number, width: number): string {
+    console.log(number);
+    console.log(width);
+
+    const str = number.toString();
+
+    if (str.length === width) {
+        return str;
+    }
+
+    if (str.length < width) {
+        const diff = width - str.length;
+        const strArr = str.split('');
+        console.log(diff);
+        for (let i = 0; i < diff; i++) {
+            strArr.unshift(0);
+        }
+        return strArr.join('');
+    }
+
+    const res = str.slice(-width);
+
+    return res;
 }
 
 console.log(integerToStringOfFixedWidth(1234, 2));
