70cfc23b7821b1e7f6d3faaed0a1181e6a3ea80a
Aman Bhardwaj <inblack>
2021-03-03 13:20:38 +0530

    integerToStringOfFixedWidth done

--- integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
@@ -1,7 +1,4 @@
-function integerToStringOfFixedWidth (number: number, width: number): string {
-    console.log(number);
-    console.log(width);
-
+function integerToStringOfFixedWidth2 (number: number, width: number): string {
     const str = number.toString();
 
     if (str.length === width) {
@@ -11,7 +8,6 @@ function integerToStringOfFixedWidth (number: number, width: number): string {
     if (str.length < width) {
         const diff = width - str.length;
         const strArr = str.split('');
-        console.log(diff);
         for (let i = 0; i < diff; i++) {
             strArr.unshift('0');
         }
@@ -22,6 +18,28 @@ function integerToStringOfFixedWidth (number: number, width: number): string {
 
     return res;
 }
+function integerToStringOfFixedWidth (number: number, width: number): string {
+    const str = number.toString();
+
+    if (str.length === width) {
+        return str;
+    }
+
+    if (str.length < width) {
+        const diff = width - str.length;
+        let padding = '';
+        for (let i = 0; i < diff; i++) {
+            padding += '0';
+        }
+        padding += str;
+        return padding;
+    }
+
+    const res = str.slice(-width);
+
+    return res;
+}
+
 
 console.log(integerToStringOfFixedWidth(1234, 2));
 console.log(integerToStringOfFixedWidth(1234, 4));
