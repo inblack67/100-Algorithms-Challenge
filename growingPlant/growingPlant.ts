c24d2f8d4f41d2bdd34fbd2be8ea3da7653857d0
Aman Bhardwaj <inblack>
2021-03-03 10:31:35 +0530

    growingPlant

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,20 @@
-function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
-
+function growingPlant2 (upSpeed: number, downSpeed: number, desiredHeight: number): number {
+    const heightInOneDay = upSpeed - downSpeed;
+    return Math.floor(desiredHeight / heightInOneDay);
 }
 
-console.log(growingPlant(100, 10, 910))
+function growingPlant (upSpeed: number, downSpeed: number, desiredHeight: number): number {
+    let days = 0;
+    let height = 0;
+    while (height < desiredHeight) {
+        height += upSpeed;
+        days++;
+        if (height < desiredHeight) {
+            height -= downSpeed;
+        }
+    }
+
+    return days;
+}
+
+console.log(growingPlant(100, 10, 910));
