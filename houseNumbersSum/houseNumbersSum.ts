4692386dc3296b0359b42578bbaa8059b6ee77fa
Aman Bhardwaj <inblack>
2021-03-03 10:37:13 +0530

    houseNumbersSum

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,12 @@
-function houseNumbersSum(inputArray: number[]): number {
-
+function houseNumbersSum (inputArray: number[]): number {
+    let sum = 0;
+    for (let i = 0; i < inputArray.length; i++) {
+        const el = inputArray[ i ];
+        if (el === 0) {
+            return sum;
+        }
+        sum += el;
+    }
 }
 
-console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
+console.log(houseNumbersSum([ 5, 1, 2, 3, 0, 1, 5, 0, 2 ]));
