42bd70cb2d0f2cfe285da7a576893d23ac1dd6a1
Aman Bhardwaj <inblack>
2021-03-04 17:47:31 +0530

    chessBoardCellColor

--- chessBoardCellColor/chessBoardCellColor.ts
+++ chessBoardCellColor/chessBoardCellColor.ts
@@ -1,5 +1,33 @@
-function chessBoardCellColor(cell1: string, cell2: string): boolean {
+const isEven = (num: number): boolean => num % 2 === 0;
+const areSimilar = (num1: number, num2: number) => {
+    if (isEven(num1) === isEven(num2)) {
+        return true;
+    }
+    return false;
+};
+function chessBoardCellColor (cell1: string, cell2: string): boolean {
 
+    const chess = {
+        A: 1,
+        B: 2,
+        C: 3,
+        D: 4,
+        E: 5,
+        F: 6,
+        G: 7,
+        H: 8
+    };
+
+    const cell1X = chess[ cell1[ 0 ] ];
+    const cell2X = chess[ cell2[ 0 ] ];
+    const cell1Y = +cell1[ 1 ];
+    const cell2Y = +cell2[ 1 ];
+
+    if (areSimilar(cell1X, cell2X) || !areSimilar(cell1Y, cell2Y)) {
+        return true;
+    }
+
+    return false;
 }
 
 console.log(chessBoardCellColor('A1', 'C3'));
