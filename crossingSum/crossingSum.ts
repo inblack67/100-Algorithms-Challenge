a3cd034d53f68b58599c5752a6c2c2107f9babc5
Aman Bhardwaj <inblack>
2021-03-04 20:30:06 +0530

    ratingThreshold

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,16 @@
-function crossingSum(matrix: number[][], a: number, b: number): number {
-  
+function crossingSum (matrix: number[][], a: number, b: number): number {
+    console.log(matrix);
+    let sum = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        const row = matrix[ i ];
+        for (let j = 0; j < matrix.length; j++) {
+            const col = matrix[ j ];
+            
+        }
+    }
+    return sum;
 }
 
-console.log(crossingSum([[1, 1, 1, 1], 
-    [2, 2, 2, 2], 
-    [3, 3, 3, 3]], 1, 3));
+console.log(crossingSum([ [ 1, 1, 1, 1 ],
+[ 2, 2, 2, 2 ],
+[ 3, 3, 3, 3 ] ], 1, 3));
--- ratingThreshold/ratingThreshold.ts
+++ ratingThreshold/ratingThreshold.ts
@@ -1,8 +1,20 @@
-function ratingThreshold(threshold: number, ratings: number[][]): number[] {
+const getSumOfElements = (rest: number[]) => rest.reduce((a, b) => a + b);
 
+function ratingThreshold (threshold: number, ratings: number[][]): number[] {
+    console.log(threshold);
+    console.log(ratings);
+    const res: number[] = [];
+    ratings.forEach((el, i) => {
+        const sum = getSumOfElements(el);
+        const avg = sum / el.length;
+        if (avg < threshold) {
+            res.push(i);
+        }
+    });
+    return res;
 }
 
-console.log(ratingThreshold(3.5, 
-    [[3, 4],
-    [3, 3, 3, 4],
-    [4]]));
+console.log(ratingThreshold(3.5,
+    [ [ 3, 4 ],
+    [ 3, 3, 3, 4 ],
+    [ 4 ] ]));
