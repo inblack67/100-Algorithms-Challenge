27ff387bbc494b7d2803b746d10688c8de067681
Aman Bhardwaj <inblack>
2021-03-04 17:20:44 +0530

    incrementalBackups

--- incrementalBackups/incrementalBackups.ts
+++ incrementalBackups/incrementalBackups.ts
@@ -1,11 +1,23 @@
-function incrementalBackups(lastBackupTime: number, changes: number[][]): number[] {
-
+function incrementalBackups (lastBackupTime: number, changes: number[][]): number[] {
+    const res: number[] = [];
+    for (let i = 0; i < changes.length; i++) {
+        const el = changes[ i ];
+        const time = el[ 0 ];
+        const id = el[ 1 ];
+        if (time > lastBackupTime) {
+            if (!res.includes(id)) {
+                res.push(id);
+            }
+        }
+    }
+    const sortedRes = res.sort((a, b) => a - b);
+    return sortedRes;
 }
 
-console.log(incrementalBackups(461620205, [[461620203, 1], 
-    [461620204, 2], 
-    [461620205, 6],
-    [461620206, 5], 
-    [461620207, 3], 
-    [461620207, 5], 
-    [461620208, 1]]));
+console.log(incrementalBackups(461620205, [ [ 461620203, 1 ],
+[ 461620204, 2 ],
+[ 461620205, 6 ],
+[ 461620206, 5 ],
+[ 461620207, 3 ],
+[ 461620207, 5 ],
+[ 461620208, 1 ] ]));
