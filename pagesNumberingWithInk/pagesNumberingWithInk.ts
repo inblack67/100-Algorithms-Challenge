02975df079cf73eac43e846ba4f650a462fe8012
Aman Bhardwaj <inblack>
2021-03-04 11:17:36 +0530

    pagesNumberingWithInk

--- pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -1,3 +1,17 @@
-function pagesNumberingWithInk(current: number, numberOfDigits: number): number {
+function pagesNumberingWithInk (current: number, numberOfDigits: number): number {
+    let digitsLeft = numberOfDigits;
+    let currentPage = current;
+    let count = 0;
 
+    while (digitsLeft > 0) {
+        currentPage += 1;
+        digitsLeft -= currentPage.toString().length;
+        count++;
+    }
+    return count;
 }
+
+const current = 1;
+const numberOfDigits = 5;
+console.log(pagesNumberingWithInk(current, numberOfDigits));
+console.log(pagesNumberingWithInk(8, 4));
--- proCategorization/proCategorization.ts
+++ proCategorization/proCategorization.ts
@@ -1,5 +1,30 @@
+type Person = {
+    name: string;
+    services: string[];
+};
+
 function proCategorization (pros: string[], preferences: string[][]): string[][][] {
-    
+    const res: string[][][] = [];
+
+    const people: Person[] = [];
+
+    pros.forEach((pro, i) => {
+        const services = preferences[ i ];
+        const person: Person = { name: pro, services };
+        people.push(person);
+    });
+
+    console.log(people);
+
+    for (let i = 0; i < people.length; i++) {
+        const el = people[ i ];
+        for (let j = 0; j < people.length; j++) {
+            const el2 = people[ j ];
+            
+        }
+    }
+
+    return res;
 }
 
 console.log(proCategorization([ "Jack", "Leon", "Maria" ], [ [ "Computer repair", "Handyman", "House cleaning" ],
